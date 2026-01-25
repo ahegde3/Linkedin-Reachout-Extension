35cd3e80e7183cdd872a9d6c3cf24c470b7d4a8d
Anish <anish@Anishs-MacBook-Air-2.local>
2026-01-24 21:51:28 -0500

    new reachout template added

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -2,8 +2,9 @@ import type { Template } from '../types'
 
 export const templates: Template[] = [
   {
-    id: 'reachout',
-    name: 'Reachout Message',
+    id: 'General Reachout Message',
+    name: 'General Reachout Message',
+    description: 'Networking & introductions',
     message: `Hi {{firstName}},
 
 My name is Anish Hegde, and I’m MS CS graduate student from Northeastern University. As I navigate my own path toward the job market, I’d greatly value the opportunity to connect.
@@ -12,15 +13,28 @@ Best regards,
 Anish Hegde `,
   },
   {
-    id: 'referral',
-    name: 'Referral Request',
+    id: 'Founder Reachout Message',
+    name: 'Founder Reachout Message',
+    description: 'Startup Founder Reachout Message',
     message: `Hi {{firstName}},
 
-I hope this message finds you well! I noticed you work at {{company}} and I'm very interested in opportunities there.
+I am a Fullstack developer who has worked with YC backed startups and have experience in working at their growth stage, building features. I am curious to learn more about your product and challenges that you are facing.
 
-Would you be open to a brief chat about your experience, or potentially referring me if there's a good fit?
+    Thanks,
+    Anish
+    Resume: https://shorturl.at/uzV2x`,
+  },
+  {
+    id: 'referral',
+    name: 'Referral Request',
+    description: 'Referral Request',
+    message: `Hi {{firstName}},
 
-Thank you for your time!`,
+      I hope this message finds you well! I noticed you work at {{company}} and I'm very interested in opportunities there.
+
+      Would you be open to a brief chat about your experience, or potentially referring me if there's a good fit?
+
+      Thank you for your time!`,
   },
 ]
 
